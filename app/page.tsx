4e02245bc60b737378aa5286613ce3dbe32c9d38
Ferenandios <www.terunplay@gmail.com>
2025-07-09 18:32:14 +0300

    Added Header-component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
-import Image from "next/image";
+import Header from "@/components/Header/Header";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center items-center w-screen h-screen bg-black">
-        <div className="flex-col justify-center">
-          <Image
-            alt="Настюша Геймерша"
-            src={"/charlotte-z.jpg"}
-            width={250}
-            height={250}
-          />
-          <h1 className="text-6xl text-center text-white mt-[30px] tracking-widest">
-            gaming
-          </h1>
-        </div>
-      </div>
+      <main>
+        <Header />
+      </main>
     </>
   );
 };
