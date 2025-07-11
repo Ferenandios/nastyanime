93951f3d1f05a41fa88eaeaace0bb00c156d3b38
Ferenandios <www.terunplay@gmail.com>
2025-07-11 09:14:02 +0300

    Added props for child-components

--- components/Messages/Message/Message.tsx
+++ components/Messages/Message/Message.tsx
@@ -1,13 +1,15 @@
 import React, { FC, JSX } from "react";
 import Info from "./Info/Info";
 import Text from "./Text/Text";
+import { type Message } from "@/types/types";
+import styles from "./Message.module.css";
 
-const Message: FC = (): JSX.Element => {
+const Message: FC<{ message: Message }> = ({ message }): JSX.Element => {
   return (
     <>
-      <div className="text-[12px] w-full bg-[#FDFFD2] p-[16px] mt-[8px] first:mt-0">
-        <Info />
-        <Text />
+      <div className={styles.message}>
+        <Info username={message.username} date={message.date} />
+        <Text text={message.text} />
       </div>
     </>
   );
