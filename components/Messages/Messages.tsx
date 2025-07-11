6dd579453670611ddae8433d6cfd0a8e830a7353
Ferenandios <www.terunplay@gmail.com>
2025-07-11 09:13:16 +0300

    Added export styles

--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -1,12 +1,18 @@
+"use client";
 import React, { FC, JSX } from "react";
 import Message from "./Message/Message";
+import { useMessages } from "../message-context";
+import styles from "./Messages.module.css";
 
 const Messages: FC = (): JSX.Element => {
+  const { messages } = useMessages();
   return (
     <>
-      <div className="flex justify-center mt-[30px]">
-        <div className="flex flex-col w-[600px]">
-          <Message />
+      <div className={styles.ansoc}>
+        <div className={styles.messages}>
+          {messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
         </div>
       </div>
     </>
