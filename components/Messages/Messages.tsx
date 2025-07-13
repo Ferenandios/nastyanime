2af0c2c927fac89b8d75427cc4f054183b07de08
Ferenandios <www.terunplay@gmail.com>
2025-07-13 17:00:10 +0300

    Deleted hiMessage

--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -6,37 +6,10 @@ import styles from "./Messages.module.css";
 
 const Messages: FC = (): JSX.Element => {
   const { messages } = useMessages();
-  const hiMessage: Message = {
-    id: "nyan",
-    username: "Николай Князев",
-    text: 'Прифффки! Оставь сообщение на этом сайте с помощью рофлинки ниже с надписью "текст"',
-    date: getFormattedDate(),
-  };
-  function getFormattedDate() {
-    const now = new Date();
-
-    // Получаем день, месяц и год (двузначный)
-    const day = String(now.getDate()).padStart(2, "0");
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const year = String(now.getFullYear()).slice(-2);
-
-    // Получаем день недели (сокращенный до 3 букв)
-    const daysOfWeek = ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Сбт"];
-    const dayOfWeek = daysOfWeek[now.getDay()];
-
-    // Получаем часы, минуты и секунды
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-
-    // Формируем строку
-    return `${day}/${month}/${year} ${dayOfWeek} ${hours}:${minutes}:${seconds}`;
-  }
   return (
     <>
       <div className={styles.ansoc}>
         <div className={styles.messages}>
-          <Message key={hiMessage.id} message={hiMessage} />
           {messages.map((message) => (
             <Message key={message.id} message={message} />
           ))}
