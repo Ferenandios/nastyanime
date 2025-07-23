b9d1bc5c78147e5cf479ca523ac9fb7b0b7516cd
Ferenandios <www.terunplay@gmail.com>
2025-07-23 22:45:53 +0300

    Created Mobile-component

---
+++ components/Mobile/Mobile.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React, { FC, JSX } from "react";
+import styles from "./Mobile.module.css";
+
+const Mobile: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className={styles.mobile}>
+        <h2 className={styles.title}>че нет компа?))</h2>
+        <Image
+          className={styles.image}
+          alt="Туалет Скибиди"
+          src="/skibidi.gif"
+          width={150}
+          height={100}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Mobile;
