524ff947824ba0adf06c26cb1e22b2789e361d9d
Ferenandios <www.terunplay@gmail.com>
2025-08-03 19:15:20 +0300

    Added auto-scroll for messages

--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -1,11 +1,19 @@
 "use client";
-import React, { FC, JSX } from "react";
-import Message from "./Message/Message";
+import { FC, JSX, useEffect, useRef } from "react";
 import { useMessages } from "../message-context";
+import Message from "./Message/Message";
 import styles from "./Messages.module.css";
 
 const Messages: FC = (): JSX.Element => {
   const { messages } = useMessages();
+  const bottomOfPanelRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bottomOfPanelRef.current) {
+      bottomOfPanelRef.current.scrollIntoView();
+    }
+  }, [messages]);
+
   return (
     <>
       <div className={styles.ansoc}>
@@ -13,6 +21,7 @@ const Messages: FC = (): JSX.Element => {
           {messages.map((message) => (
             <Message key={message.id} message={message} />
           ))}
+          <div ref={bottomOfPanelRef}></div>
         </div>
       </div>
     </>
