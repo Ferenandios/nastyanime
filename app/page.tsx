4433ea740c8fb15f277249015cc4da65807f0d13
Ferenandios <www.terunplay@gmail.com>
2025-08-09 18:29:50 +0300

    Getting device by user agent now

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,39 @@
+"use client";
 import Background from "@/components/Background/Background";
 import Header from "@/components/Header/Header";
 import Input from "@/components/Input/Input";
 import { MessageProvider } from "@/components/message-context";
 import Messages from "@/components/Messages/Messages";
 import Mobile from "@/components/Mobile/Mobile";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Ensure code runs only in the browser
+      const userAgent = navigator.userAgent;
+      const mobileRegex =
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i;
+      setIsMobile(mobileRegex.test(userAgent));
+    }
+  }, []);
   return (
     <>
       <main>
         <Background />
         <Header />
-        <MessageProvider>
-          <div className="hidden fixed bottom-[100px] lg:block">
-            <Messages />
-            <Input />
-          </div>
+        {isMobile ? (
           <Mobile />
-        </MessageProvider>
+        ) : (
+          <MessageProvider>
+            <div className="fixed bottom-[100px]">
+              <Messages />
+              <Input />
+            </div>
+          </MessageProvider>
+        )}
       </main>
     </>
   );
